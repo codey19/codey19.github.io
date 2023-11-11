cd7c867f5b95201892668ef5bc9b98daa42cc803
code19 <cqsheng19@gmail.com>
2023-11-10 20:49:08 -0500

    jio

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -5,7 +5,7 @@ import { BackTopLink } from './BackTopLink';
 
 const Skills = () => {
   return (
-    <section className='py-20 bg-emerald-50 align-element ' id='skills'>
+    <section className='py-20 bg-emerald-100 align-element ' id='skills'>
       <SectionTitle text='tech stack' />
       <div className='py-16 grid gap-8 md:grid-cols-2 lg:grid-cols-3'>
         {skills.map((skill) => {
